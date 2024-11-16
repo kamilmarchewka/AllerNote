4a9c94c7754bf7f0572510ad1437ebc0a916f062
Kamil <kamilm2003@gmail.com>
2024-11-16 21:17:18 +0100

    init

---
+++ app/about-me/page.js
@@ -0,0 +1,28 @@
+//  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+import Image from "next/image";
+
+import Nav from "@/components/nav/Nav";
+
+export default function Home() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link>asdf</Link>
+        </li>
+        <li>
+          <Link>asdf</Link>
+        </li>
+        <li>
+          <Link>asdf</Link>
+        </li>
+        <li>
+          <Link>asdf</Link>
+        </li>
+        <li>
+          <Link>asdf</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
