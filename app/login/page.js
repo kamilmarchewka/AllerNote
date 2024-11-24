38e96b6edc0e730a94bf3ecbfa26c3b33c8a93cd
Kamil <kamilm2003@gmail.com>
2024-11-24 11:06:22 +0100

    Add: Dandelions

--- app/login/page.js
+++ app/login/page.js
@@ -6,11 +6,18 @@ import InputBox from "@/components/login/InputBox";
 import GoBackIcon from "@/components/login/GoBackIcon";
 import Card from "@/components/login/Card";
 import Form from "@/components/login/Form";
+import Dandelion from "@/components/login/Dandelion";
 
 export default function Login() {
   return (
-    <section className=" flex items-center pt-32 pb-14 lg:min-h-[calc(100vh-40px)]">
+    <section className="flex items-center pt-32 pb-14 lg:min-h-[calc(100vh-40px)]">
       {/* Background svg */}
+      <div className="invisible md:visible fixed bottom-0 left-[8vw]">
+        <Dandelion />
+      </div>
+      <div className="invisible md:visible fixed -bottom-14 left-[13vw] transform origin-bottom rotate-[30deg]">
+        <Dandelion />
+      </div>
 
       {/* Login in form */}
       <Card title="Zaloguj się">
--- app/rejestracja/page.js
+++ app/rejestracja/page.js
@@ -3,11 +3,18 @@ import Card from "@/components/login/Card";
 import GoBackIcon from "@/components/login/GoBackIcon";
 import Form from "@/components/login/Form";
 import Link from "next/link";
+import Dandelion from "@/components/login/Dandelion";
 
 export default function Registration() {
   return (
     <section className=" flex items-center pt-32 pb-14 lg:min-h-[calc(100vh-40px)]">
       {/* Background svg */}
+      <div className="invisible md:visible fixed bottom-0 left-[8vw]">
+        <Dandelion />
+      </div>
+      <div className="invisible md:visible fixed -bottom-14 left-[13vw] transform origin-bottom rotate-[30deg]">
+        <Dandelion />
+      </div>
 
       {/* Registration form */}
       <Card title="Rejestracja">
