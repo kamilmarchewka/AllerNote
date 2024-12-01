29e79f6a42f22ef6d2e9d7bc0899df7c411806f8
Kamil <kamilm2003@gmail.com>
2024-12-01 12:13:34 +0100

    Add: Label that shows allergen intensity

---
+++ app/alergeny/page.js
@@ -0,0 +1,10 @@
+import IntensityLabel from "@/components/alergeny/IntensityLabel";
+import React from "react";
+
+export default function Allergens() {
+  return (
+    <div className="mt-32">
+      <IntensityLabel />
+    </div>
+  );
+}
