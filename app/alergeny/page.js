675a2902f01c2b81fdf049f556207b9a5216bf9a
Kamil <kamilm2003@gmail.com>
2024-12-01 12:29:07 +0100

    Add: Voivodeship select input markup

--- app/alergeny/page.js
+++ app/alergeny/page.js
@@ -1,10 +1,20 @@
 import IntensityLabel from "@/components/alergeny/IntensityLabel";
+import VoivodeshipSelect from "@/components/alergeny/VoivodeshipSelect";
 import React from "react";
 
 export default function Allergens() {
   return (
-    <div className="mt-32">
-      <IntensityLabel />
-    </div>
+    <>
+      <header className="text-4xl font-semibold mb-16 mt-32 lg:mt-44">
+        <h1 className="flex gap-4 flex-wrap">
+          <span>Co teraz pyli w</span>
+          <span className="flex gap-4">
+            <VoivodeshipSelect />
+            <span>?</span>
+          </span>
+        </h1>
+      </header>
+      <section></section>
+    </>
   );
 }
---
+++ components/alergeny/VoivodeshipSelect.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function VoivodeshipSelect() {
+  return (
+    <div>
+      <select>
+        <option>Malopolskim</option>
+        <option>Lubuskim</option>
+        <option>Podlaskim</option>
+      </select>
+    </div>
+  );
+}
