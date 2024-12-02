946a2f1ab785cbc56567f5cce2aa22f0a1673a69
Kamil <kamilm2003@gmail.com>
2024-12-02 21:36:45 +0100

    Update: Nav links

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -6,6 +6,7 @@ import Hamburger from "./Hamburger";
 import LoginButton from "./LoginButton";
 
 export default function Nav() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [navIsOpen, setNavIsOpen] = useState(false);
   const [submenuIsOpen, setSubmenuIsOpen] = useState(false);
   const submenuRef = useRef(null);
@@ -45,14 +46,16 @@ export default function Nav() {
               : "translate-x-[calc(100%+1.1rem)] opacity-0 md:translate-x-0 md:opacity-100"
           } transition-all md:flex-row md:gap-3 md:p-0 md:items-center md:static md:bg-transparent md:shadow-none md:rounded-none`}
         >
+          {isLoggedIn && (
+            <li className="block">
+              <Link href="/" className="block w-full p-2 text-left">
+                Kalendarz
+              </Link>
+            </li>
+          )}
           <li className="block">
-            <Link href="/" className="block w-full p-2 text-left">
-              Dashboard
-            </Link>
-          </li>
-          <li className="block">
-            <Link href="/kalendarz" className="block w-full p-2 text-left">
-              Kalendarz
+            <Link href="/alergeny" className="block w-full p-2 text-left">
+              Co pyli?
             </Link>
           </li>
           <li className="flex flex-col-reverse md:relative">
@@ -60,8 +63,11 @@ export default function Nav() {
             <div className="flex justify-center border-t pt-4 mt-2 md:pt-0 md:mt-0 md:ml-4 md:peer">
               <LoginButton
                 ref={submenuRef}
-                onClick={() => setSubmenuIsOpen((prev) => !prev)}
-                isLoggedIn={true}
+                onClick={() => {
+                  setIsLoggedIn(true);
+                  if (isLoggedIn) setSubmenuIsOpen((prev) => !prev);
+                }}
+                isLoggedIn={isLoggedIn}
                 username="Maksumilian Łuczak"
               />
             </div>
@@ -78,9 +84,19 @@ export default function Nav() {
                   Ustawienia
                 </Link>
               </li>
-              <li className="block">
-                <button className="block w-full p-2 text-left">Wyloguj</button>
-              </li>
+              {isLoggedIn && (
+                <li className="block">
+                  <button
+                    onClick={() => {
+                      console.log("alskdfj");
+                      setIsLoggedIn(false);
+                    }}
+                    className="block w-full p-2 text-left"
+                  >
+                    Wyloguj
+                  </button>
+                </li>
+              )}
             </ul>
           </li>
         </ul>
