2e9baf3072077129c371f26a5d78334b4aabe4df
Kamil <kamilm2003@gmail.com>
2024-12-05 10:38:14 +0100

    Restructure: Calendar

--- app/kalendarz/page.js
+++ app/kalendarz/page.js
@@ -7,15 +7,18 @@ import SymptomsIntensity_note from "@/components/note/symptoms-note";
 import { Calendar } from "@/components/calendar/Calendar";
 
 export default function Home() {
-  const [currentDate, setCurrentDate] = useState("");
-  const [selectedDateStr, setSelectedDateStr] = useState("");
-
-  console.log("selected data str", selectedDateStr);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <section className="flex flex-col items-center gap-28  pt-32 pb-10 lg:min-h-[calc(100vh-40px)] md:flex-row md:gap-8 lg:gap-20 md:justify-center md:items-start">
-      <Calendar updateSelectedDate={setSelectedDateStr} />
-      <SymptomsIntensity_note selectedDateStr={selectedDateStr} />
+      <Calendar
+        setCurrentMonth={setCurrentMonth}
+        setSelectedDate={setSelectedDate}
+        currentMonth={currentMonth}
+        selectedDate={selectedDate}
+      />
+      <SymptomsIntensity_note selectedDateStr={"aaa"} />
     </section>
   );
 }
