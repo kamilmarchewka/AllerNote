adb194014742efe42ab21938123661b9c43a4adb
Kamil <kamilm2003@gmail.com>
2024-12-05 13:58:54 +0100

    Remove: gsap d animation

--- app/not-found.js
+++ app/not-found.js
@@ -4,28 +4,35 @@ import React from "react";
 import Dandelion from "@/components/login/Dandelion";
 
 import gsap from "gsap";
+// import CustomEase from "gsap/CustomEase";
 import { useGSAP } from "@gsap/react";
-gsap.registerPlugin(useGSAP);
+// gsap.registerPlugin(useGSAP);
+// gsap.registerPlugin(CustomEase);
 import { useRef } from "react";
 import Header from "@/components/404/Header";
 
 export default function Custom404() {
   const container = useRef(null);
 
-  useGSAP(
-    () => {
-      // gsap code here...
-      // gsap.to(".aaa", {
-      //   xPercent: 500,
-      //   yPercent: -300,
-      //   scale: 0.3,
-      //   rotate: 190,
-      //   duration: 2,
-      //   delay: 1,
-      // });
-    },
-    { scope: container }
-  ); // <-- easily add a scope for selector text (optional)
+  // useGSAP(
+  //   () => {
+  //     gsap.to(".fly", {
+  //       scale: 0.2,
+  //       // autoAlpha: 0,
+  //       rotate: 150,
+  //       x: 600,
+  //       y: -300,
+  //       duration: 2.5,
+  //       autoAlpha: 0,
+  //       delay: 10,
+  //       ease: CustomEase.create(
+  //         "custom",
+  //         "M0,0 C0.277,0.495 0.38,-0.306 0.467,-0.151 0.506,-0.115 0.538,0.079 0.599,0.157 0.634,0.203 0.692,-0.161 0.761,-0.146 0.825,0.67 0.799,0.88 0.965,1 1.023,1.042 0.973,1 1,1 "
+  //       ),
+  //     });
+  //   },
+  //   { scope: container }
+  // ); //
 
   return (
     <section
@@ -41,8 +48,10 @@ export default function Custom404() {
       <div className="w-[10rem] invisible md:visible fixed -bottom-14 left-[30vw] transform origin-bottom rotate-[30deg] animate-[dandelion-2_17s_ease-in-out_infinite]">
         <Dandelion />
       </div>
-      <div className="aaa w-[14rem] invisible md:visible fixed -bottom-14 left-[45vw] transform origin-bottom -rotate-[7deg] animate-[dandelion-3_17s_ease-in-out_infinite]  -z-10">
-        <Dandelion />
+      <div className=" w-[14rem] invisible md:visible fixed -bottom-28 left-[45vw] transform origin-bottom -rotate-[7deg] animate-[dandelion-3_17s_ease-in-out_infinite]  -z-10">
+        <div className="fly">
+          <Dandelion />
+        </div>
       </div>
       <div className="w-[11.5rem] invisible md:visible fixed -bottom-14 left-[49vw] transform origin-bottom rotate-[20deg] animate-[dandelion-4_15s_ease-in-out_infinite]">
         <Dandelion />
