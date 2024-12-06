12d67d9b2cb6071e7de7a665eb3dd12c039fbd78
Kamil <kamilm2003@gmail.com>
2024-12-06 20:01:37 +0100

    Add: Dandelion opacity/depth

--- components/404/Header.js
+++ components/404/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
+import ButtonPrimary from "../buttons/ButtonPrimary";
 
 export default function Header() {
   return (
-    <header className="text-[20rem] font-bold ">
-      <h1 className="flex gap-1 text-[20rem] font-bold text-eden-700">
-        <span className="animate-[404-jump_9s_ease-in-out_infinite_1s]">4</span>
-        <span className="animate-[404-jump_9s_ease-in-out_infinite_1.1s]">
+    <header className="relative z-[1] flex flex-col items-center ">
+      <h1 className="flex  text-eden-700 text-[20rem] leading-none font-bold ">
+        <span className="block animate-[404-jump_9s_ease-in-out_infinite_1s]">
+          4
+        </span>
+        <span className="block animate-[404-jump_9s_ease-in-out_infinite_1.1s]">
           0
         </span>
-        <span className="animate-[404-jump_9s_ease-in-out_infinite_1.2s]">
+        <span className="block animate-[404-jump_9s_ease-in-out_infinite_1.2s]">
           4
         </span>
       </h1>
+      <div>
+        <ButtonPrimary>Powrót na stronę główną</ButtonPrimary>
+      </div>
     </header>
   );
 }
