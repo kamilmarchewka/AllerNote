103a07dcb9668ced1dc08d85cbf85f80dab8eb7f
Kamil <kamilm2003@gmail.com>
2024-12-06 20:05:12 +0100

    Add back to / page btn

--- components/404/Header.js
+++ components/404/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ButtonPrimary from "../buttons/ButtonPrimary";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="relative z-[1] flex flex-col items-center ">
+    <header className="relative z-[1] flex flex-col gap-10 items-center ">
       <h1 className="flex  text-eden-700 text-[20rem] leading-none font-bold ">
         <span className="block animate-[404-jump_9s_ease-in-out_infinite_1s]">
           4
@@ -16,7 +16,13 @@ export default function Header() {
         </span>
       </h1>
       <div>
-        <ButtonPrimary>Powrót na stronę główną</ButtonPrimary>
+        <Link
+          href="/"
+          className="block px-[2.1rem] py-[.7rem]  rounded-[1.13rem] transform hover:scale-105 transition-transform bg-eden-700 text-white
+          "
+        >
+          Powrót na stronę główną
+        </Link>
       </div>
     </header>
   );
