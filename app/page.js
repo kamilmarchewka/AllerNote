78759681aa78fabd62ee43ff24361f4586311aca
maksluczak <maksluczak99@gmail.com>
2024-12-18 18:28:15 +0100

    i can make money from the comfort of my sofa

--- app/page.js
+++ app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <section className="flex flex-col gap-28 pt-32 pb-10 items-center">
       <section className="flex flex-col md:flex-row">
-      <div className="relative text-center font-bold text-[40vw] text-white outlines font-outline-2 m-10 lg:m-60 md:m-40 sm:m-20">
-          Note
+      <div className="relative text-center font-bold text-[40vw] text-white m-10 lg:m-60 md:m-40 sm:m-20">
+          <h1 className="outlines font-outline-2">Note</h1>
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 text-center font-bold text-eden-700 text-[20vw]">
-          Aller
+          <h1>Aller</h1>
         </div>
       </div>
       </section>
