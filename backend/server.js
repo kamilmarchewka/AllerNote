f6677ee517597105b1b664548fcb429a569d9d70
maksluczak <maksluczak99@gmail.com>
2025-01-03 18:11:41 +0100

    pierwsze podejscie do logow, poprawienie serwera

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,23 @@
 const express = require('express');
+const logEvents = require('./utils/logEvents');
 const app = express();
 
+app.use((req, res, next) => {
+    logEvents(`${req.method}\t${req.url}`);
+    next();
+});
+
+// Routes
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
-
-// app.get('/', (req, res) => {
-//     res.send('<h1>hello route node</h1>');
-// });
-
-// const usersRoute = require('./routes/users');
-// const productsRoute = require('./routes/products');
 
 app.get('/', (req, res) => {
     res.send('<h1>Home page</h1>');
 });
 
-app.use('./', (req, res) => {
+app.use('*', (req, res) => {
     res.status(404).send('<h1>Not found</h1>');
-})
-
-// app.get('/kalendarz', (req, res) => {
-//     res.send('<h1>Calendar</h1>');
-// });
-
-// app.get('/alergeny', (req, res) => {
-//     res.send('<h1>Allergen</h1>');
-// });
-
-// app.get('/login', (req, res) => {
-//     res.send('<h1>Login page</h1>');
-// });
-
-// app.get('/rejestracja', (req, res) => {
-//     res.send('<h1>Register page</h1>');
-// });
-
-// app.use((req, res) => {
-//     res.status(404).send('<h1>404 not found</h1>');
-// })
+});
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => logEvents(`Log event emitted`));
