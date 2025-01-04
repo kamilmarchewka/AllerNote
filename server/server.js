dc30dbdc4a514ff4fe46ae8834baa1f0468ac792
maksluczak <maksluczak99@gmail.com>
2025-01-04 17:07:00 +0100

    a

--- server/server.js
+++ server/server.js
@@ -1,29 +1,32 @@
 const express = require('express');
+const path = require('path');
 const logEvents = require('./utils/logEvents');
 const app = express();
-const EventEmitter = require('events');
-class Emitter extends EventEmitter { };
 
-const MyEmitter = new Emitter();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '..', 'app')));
 
 app.use((req, res, next) => {
     logEvents(`${req.method}\t${req.url}`);
     next();
 });
 
-// Routes
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1>');
+    res.sendFile(path.join(__dirname, '..', 'client', 'app', 'page.js'));
+});
+
+app.get('/kalendarz', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'app', 'kalendarz', 'page.js'));
 });
 
 app.use('*', (req, res) => {
     res.status(404).send('<h1>Not found</h1>');
 });
 
-const port = process.env.PORT || 3001;
 app.listen(port, () => {
     logEvents(`Log event emitted`);
     console.log(`Server running on port ${port}`);
