fb7451801b1d31a21b1e32ce9ad42f980059dba7
maksluczak <maksluczak99@gmail.com>
2025-01-09 10:35:47 +0100

    a

---
+++ data/migrations/0002_closed_pyro.sql
@@ -0,0 +1,14 @@
+ALTER TABLE "user" RENAME TO "users";--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "user_username_unique";--> statement-breakpoint
+ALTER TABLE "users" DROP CONSTRAINT "user_email_unique";--> statement-breakpoint
+ALTER TABLE "notes" DROP CONSTRAINT "notes_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "user_location" DROP CONSTRAINT "user_location_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "user_symptoms" DROP CONSTRAINT "user_symptoms_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "notes" ADD CONSTRAINT "notes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_location" ADD CONSTRAINT "user_location_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_symptoms" ADD CONSTRAINT "user_symptoms_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_username_unique" UNIQUE("username");--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");
