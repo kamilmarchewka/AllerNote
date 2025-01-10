03195f7dd12d93020ccde689989fd6e0c39ec767
Kamil <kamilm2003@gmail.com>
2025-01-10 15:15:12 +0100

    Update: Landing page
    Add: Dolacz do nas juz dzis btn

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
-//  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
-"use client";
-
 import Services from "@/components/welcome/Services";
 import WelcomeBadge from "@/components/welcome/WelcomeBadge";
-import MainAllerNote from "@/components/welcome/MainAllerNote";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="flex flex-col gap-28 pt-32 pb-10 items-center">
-        <MainAllerNote />
-        <WelcomeBadge />
-        <Services />
+    <section className="flex flex-col items-center justify-center gap-20 pt-32 pb-10 min-h-[calc(100vh-40px)]">
+      <header className="relative text-center font-bold text-white -z-[1] select-none">
+        <h1 className="text-[20vw] text-eden-700 leading-none">Aller</h1>
+        <h1 className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 outlines font-outline-2 text-transparent text-[35vw] tracking-wider">
+          Note
+        </h1>
+      </header>
+      <div>
+        <Link
+          href="/rejestracja"
+          className="block px-[2.1rem] py-[.7rem]  rounded-[1.13rem] bg-eden-700 text-white transform hover:scale-105 transition-transform "
+        >
+          Dołącz do nas już dziś!
+        </Link>
+      </div>
+      {/* <WelcomeBadge />
+      <Services /> */}
     </section>
   );
 }
