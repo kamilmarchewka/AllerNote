92335be93b4f8bd5541c5633dabcf3e3d9e74e59
Kamil <kamilm2003@gmail.com>
2025-01-10 13:28:04 +0100

    Add: Connect db with the project, make basic request

---
+++ app/api/cities/route.js
@@ -0,0 +1,19 @@
+import { query } from "../../../lib/db";
+
+export async function GET() {
+  try {
+    const cities = await query("SELECT * FROM city;");
+    return new Response(JSON.stringify(cities), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Database error:", error.message);
+    console.error("Stack trace:", error.stack);
+
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
--- app/page.js
+++ app/page.js
@@ -4,13 +4,27 @@
 import Services from "@/components/welcome/Services";
 import WelcomeBadge from "@/components/welcome/WelcomeBadge";
 import MainAllerNote from "@/components/welcome/MainAllerNote";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/api/cities");
+      const data = await response.json();
+      setUsers(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log(users);
+
   return (
     <section className="flex flex-col gap-28 pt-32 pb-10 items-center">
-        <MainAllerNote />
-        <WelcomeBadge />
-        <Services />
+      <MainAllerNote />
+      <WelcomeBadge />
+      <Services />
     </section>
   );
 }
