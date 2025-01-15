cfce3f9969572e4bd3bcb9198ce5945e8e03f70e
maksluczak <maksluczak99@gmail.com>
2025-01-15 18:53:24 +0100

    a

--- app/ustawienia/page.js
+++ app/ustawienia/page.js
@@ -9,8 +9,11 @@ export default function Home() {
       <header className="grid gap-3 py-5 px-4 grid-cols-2 lg:grid-cols-[1fr_.7fr] border-b border-gray-200 text-4xl font-bold">
         Ustawienia
       </header>
+
       <div className="py-5 px-4">
-        <label className="text-gray-800 text-2xl font-bold block mb-2">Nazwa użytkownika</label>
+        <label className="text-gray-800 text-2xl font-bold block mb-2">
+          Nazwa użytkownika
+        </label>
         <div className="relative flex items-center">
           <input
             name="username"
@@ -21,20 +24,26 @@ export default function Home() {
           <div className="px-10"></div>
         </div>
       </div>
+
       <div className="py-5 px-4">
-        <label className="text-gray-800 text-2xl font-bold block mb-2">Email</label>
+        <label className="text-gray-800 text-2xl font-bold block mb-2">
+          Email
+        </label>
         <div className="relative flex items-center">
           <input
             name="email"
             type="text"
             className="w-full text-gray-800 text-base pl-2 pr-8 py-3 outline-none bg-transparent border-b-2 border-black focus:ring-0 focus:border-black"
-            placeholder="aa@gmail.com"
+            placeholder="galson@gmail.com"
           />
           <div className="px-10"></div>
         </div>
       </div>
+
       <div className="py-5 px-4">
-        <label className="text-gray-800 text-2xl font-bold block mb-2">Hasło</label>
+        <label className="text-gray-800 text-2xl font-bold block mb-2">
+          Hasło
+        </label>
         <div className="relative flex items-center">
           <input
             name="password"
@@ -44,9 +53,9 @@ export default function Home() {
           />
           <div className="px-10"></div>
         </div>
-        <div className="relative flex items-center">
+        <div className="relative flex items-center mt-4">
           <input
-            name="password"
+            name="new-password"
             type="text"
             className="w-full text-gray-800 text-base pl-2 pr-8 py-3 outline-none bg-transparent border-b-2 border-black focus:ring-0 focus:border-black"
             placeholder="Wprowadź nowe hasło"
@@ -54,10 +63,13 @@ export default function Home() {
           <div className="px-10"></div>
         </div>
       </div>
+
       <div className="py-5 px-4">
-        <label className="text-gray-800 text-2xl font-bold block mb-2">Województwo</label>
+        <label className="text-gray-800 text-2xl font-bold block mb-2">
+          Województwo
+        </label>
         <div className="flex items-center gap-2 md:gap-4">
-            <VoivodeshipSelect2 />
+          <VoivodeshipSelect2 />
         </div>
       </div>
     </section>
