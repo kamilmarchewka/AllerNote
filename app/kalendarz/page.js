d6134c30f32e3a394e05d0cd1a8462e5a3247457
Kamil <kamilm2003@gmail.com>
2025-01-21 12:46:12 +0100

    Firebase setup

--- app/kalendarz/page.js
+++ app/kalendarz/page.js
@@ -1,12 +1,9 @@
-//  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
 "use client";
 
 import React, { useState } from "react";
 import SymptomsNote from "@/components/note/SymptomsNote";
-
 import { Calendar } from "@/components/calendar/Calendar";
 import Badge from "@/components/badge/Badge";
-
 
 export default function Home() {
   const [currentMonth, setCurrentMonth] = useState(new Date());
